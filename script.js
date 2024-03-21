7b7c0f6425ff9d261af205e5600c42d51fb01163
gmanatov <jfexpr@gmail.com>
2024-03-21 15:28:27 -0500

    introduced mechanic to randomize colons without repetitions as well but it doesnt work everytime and sometimes gives me undefineds

--- script.js
+++ script.js
@@ -8,15 +8,30 @@ let arrLottery = (arr) => {//This function takes array and gets random element o
 let cells = []
 
 
-let arrY = [1, 2, 3, 4, 5, 6, 7, 8, 9]//Array for checking column repetitions
-
 for (let i = 0; i < 9; i++){
     cells[i] = [];
-    let arrX = [1, 2, 3, 4, 5, 6, 7, 8, 9]//array for checking row repetitions
+    let arrX = [1, 2, 3, 4, 5, 6, 7, 8, 9]//Array for checking row repetitions
     for (let j = 0; j < 9; j++){
         cells[i][j] = arrLottery(arrX);
+        
+        let k = 0;
+        if (i > 0){
+            let arrY = arrX.slice();
+            while (k < i){
+                if (cells[k][j] == cells[i][j]){
+                    arrY.splice((arrY.indexOf(cells[i][j])), 1);
+                    cells[i][j] = arrLottery(arrY);
+                    k = 0;
+                }
+                else {
+                    k++;
+                }
+            }
+        }
+        
         arrX.splice((arrX.indexOf(cells[i][j])), 1);
     }
+    
 }
 
 //Outputting generated array in console
